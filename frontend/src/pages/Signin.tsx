94ef0c45db1003828ecba9f07b5ccb3b07e44970
Piyushzingade <piyushzingade@gmail.com>
2024-09-22 01:19:57 +0530

    Updated

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,8 +1,15 @@
+import { Auth } from "../componenets/Auth";
+import { Quote } from "../componenets/Quote";
 
-export  const Signin = ()=> {
+export const Signin = () => {
   return (
-    <div>Signin</div>
-  )
-}
-
- 
+    <div className="grid grid-cols-1 lg:grid-cols-2">
+      <div className="">
+        <Auth type="signin" />
+      </div>
+      <div className="hidden lg:block">
+        <Quote />
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,13 +3,13 @@ import { Quote } from "../componenets/Quote"
 
 export const Signup = ()=>{
     return (
-        <div className="grid grid-cols-2">
-            <div className="">
-                <Auth type="signin"/>     
-            </div>
-            <div className="invisible lg:visible">
-                <Quote/>
-            </div>
+      <div className="grid grid-cols-2">
+        <div className="">
+          <Auth type="signup" />
         </div>
-    )
+        <div className="hidden lg:block">
+          <Quote />
+        </div>
+      </div>
+    );
 } 
